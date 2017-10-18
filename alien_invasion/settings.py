75efb6b4a0dec23ee39c728f7c668419d7bd3691
Monkey <monkey@gmail.com>
2017-10-18 11:02:06 +0800

    gif

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,8 +3,8 @@ class Settings:
     """设置类"""
 
     def __init__(self):
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 600
+        self.screen_height = 400
         self.screen_color = (60, 60, 60)
 
         self.bullet_width = 2
